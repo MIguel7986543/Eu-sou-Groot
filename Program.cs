3bd72563449d713aa5494f48696d469ae42af829
miguel dias <mdias1300@gmail.com>
2025-04-25 21:24:34 -0300

    fazendo atividade

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Clear();
+
+string nomeDgitado = "eu sou Groot";
+
+Console.WriteLine("Conversa com seu amigo Groot");
+nomeDgitado = Console.ReadLine()!;
+
+Console.ReadLine();
+Console.WriteLine("Eu sou Groot");
+Console.ReadLine();
+Console.WriteLine("Eu sou Groot");
+Console.ReadLine();
+Console.WriteLine("Eu sou Groot");
+Console.ReadLine();
+Console.WriteLine("Eu sou Groot");
+Console.ReadLine();
+Console.WriteLine("Eu sou Groot");
+
+Console.WriteLine("Tchau Groot");
+Console.ReadKey();
+Console.WriteLine("Eu sou Groot");
